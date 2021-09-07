fa6cebdd931a06f8307a7948cfd880f89888ebb5
Gustavo <gustavo.fonseca@outlook.pt>
2021-09-07 13:48:02 +0100

    📦 NEW: Deployment with ansible

--- deployment.sh
+++ deployment.sh
@@ -2,10 +2,23 @@
 #
 # Deployment of Flask App
 
+# Create VM with Static IP
+az vm create \
+    --name "ppp_app" \
+    --image UbuntuLTS \
+    --admin-username mota-engil \
+    --generate-ssh-keys \
+    --public-ip-address PPPIpAdress \
+    --public-ip-address-allocation static \
+    --output none
+
+APP_VM_IP=$(az network public-ip show --name PPPIpAdress --query ipAddress -o tsv)
+
+# Prepare variables to be used in sed
 sed_primary_endpoint_key=$(echo $PRIMARY_ENDPOINT_KEY | sed 's/&/\\\&/g;s/\//\\\//g')
 sed_scoring_uri=$(echo $SCORING_URI | sed 's/&/\\\&/g;s/\//\\\//g')
 
-# Create the .env file for deploying to the server
+# Create the vars file for deploying to the server
 sed "s/FLASK_SECRET/${SECRET_KEY}/; \
     s/MAIL_PLACEHOLDER/${MAIL_USERNAME}/; \
     s/MAIL_PWD_PLACEHOLDER/${MAIL_PASSWORD}/; \
@@ -14,4 +27,17 @@ sed "s/FLASK_SECRET/${SECRET_KEY}/; \
     s/AZURE_USER_PLACEHOLDER/${LOGIN_INPUT}/; \
     s/AZURE_PWD_PLACEHOLDER/${PASSWORD_INPUT}/; \
     s/AZURE_SURI_PLACEHOLDER/${sed_scoring_uri}/; \
-    s/AZURE_EKEY_PLACEHOLDER/${sed_primary_endpoint_key}/;" .env_template > .env
+    s/AZURE_EKEY_PLACEHOLDER/${sed_primary_endpoint_key}/; \
+    s/APP_DOMAIN_NAME/${APP_DOMAIN_NAME}/; \
+    s/SSL_EMAIL/${SSL_EMAIL}/" ansible/group_vars/all_template > ansible/group_vars/all
+
+sed "s/IP/${APP_VM_IP}/" ansible/hosts_template > ansible/hosts
+
+cd ansible/
+
+ansible-playbook -i hosts machine_initial_setup.yml
+ansible-playbook -i hosts install_nginx_supervisor.yml
+ansible-playbook -i hosts install_app.yml
+
+
+
